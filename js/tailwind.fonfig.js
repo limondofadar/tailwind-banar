1fca9e4db60d97883f7ce6f29c841e18d41c5c5d
limondofadar <zahiddofadar@gmail.com>
2023-02-02 17:24:43 +0600

    first commit

---
+++ js/tailwind.fonfig.js
@@ -0,0 +1,60 @@
+
+
+tailwind.config = {
+    theme: {
+      extend: {
+
+        colors: {
+        navColor:'rgba(22, 12, 109, 0.5);',
+        menuHover:'#1BBF00',
+        banarOverly:'rgba(106, 98, 197, 0.9)',
+        banarHead:'#fafafa',
+        banarPara:'#E6E5F3',
+        },
+       
+
+        width: {
+            contain: '1170px',
+          },
+
+          fontFamily: {
+           
+           open: ['font-family', 'Open Sans, sans-serif'],
+           myfont: ['font-family', 'Miniver, cursive'],
+           pamrika: ['Paprika', 'cursive'],
+
+    },
+
+    borderRadius: {
+        navButton: '100px',
+       
+      },
+
+      backgroundImage: {
+       'banarImages' :"url('images/banar.jpg')",
+       
+      },
+      padding: {
+        bnarTop: '270px',
+        bnarBottom: '250px',
+        banarPara:'350px',
+      },
+      fontSize: {
+        banarH: '84px',
+        
+      }
+    
+
+      },
+      
+    },
+  };
+
+ 
+
+
+
+
+
+
+
